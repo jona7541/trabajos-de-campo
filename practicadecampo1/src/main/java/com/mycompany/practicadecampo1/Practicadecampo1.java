24927926b3d9a4c05dfad2487447ae74bf585002
jona7541 <jhonarribasplata2@gmail.com>
2026-04-30 17:49:00 -0500

    se agrego el main

--- practicadecampo1/src/main/java/com/mycompany/practicadecampo1/Practicadecampo1.java
+++ practicadecampo1/src/main/java/com/mycompany/practicadecampo1/Practicadecampo1.java
@@ -11,6 +11,32 @@ package com.mycompany.practicadecampo1;
 public class Practicadecampo1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        persona p1= new persona();
+        p1.tipo_documento= "DNI";
+        p1.nro_docuemnto= "75729341";
+        p1.paterno=" Diaz";
+        p1.materno= "Arribasplata";
+        p1.nombre= "John";
+        p1.edad=21;
+        
+        p1.verDatos();
+        
+        persona p2= new persona();
+        p2.tipo_documento= "CE";
+        p2.nro_docuemnto= "75748256";
+        p2.paterno= "Vasquez";
+        p2.materno= "Rivera";
+        p2.nombre= "Maria";
+        p2.edad=40;
+        p2.verDatos();
+        
+        persona p3= new persona();
+        p3.tipo_documento= "DNI";
+        p3.nro_docuemnto= "75729341";
+        p3.paterno= "Campos";
+        p3.materno= "Aliaga";
+        p3.nombre= "Luna";
+        p3.edad=71;
+        p3.verDatos();
     }
 }
